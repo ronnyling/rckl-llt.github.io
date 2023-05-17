6a5a512349873dddc40bade753277ca00e4d080c
r.a.ling <r.a.ling@accenture.com>
2023-05-17 14:33:57 +0800

    Update map html

--- Lelongtips/sherlock-dms/resources/restAPI/Main/Main.py
+++ Lelongtips/sherlock-dms/resources/restAPI/Main/Main.py
@@ -70,6 +70,23 @@ class Main(object):
                 <!--[if lte IE 8]><link rel="stylesheet" href="https://cdn.leafletjs.com/leaflet-0.7.2/leaflet.ie.css" /><![endif]-->
             
                 <link rel="stylesheet" href="../Lelongtips/sherlock-dms/resources/components/leaflet-sidebar.css" />
+            
+                <style>
+                    body {
+                        padding: 0;
+                        margin: 0;
+                    }
+            
+                    html, body, #map {
+                        height: 100%;
+                        font: 10pt "Helvetica Neue", Arial, Helvetica, sans-serif;
+                    }
+            
+                    .lorem {
+                        font-style: italic;
+                        color: #AAA;
+                    }
+                </style>
             </head>
             <body>
                 <div id="sidebar" class="sidebar collapsed">
@@ -119,6 +136,27 @@ class Main(object):
                         </div>
                     </div>
                 </div>
+            
+                <div id="map" class="sidebar-map"></div>
+            
+                <a href="https://github.com/Turbo87/sidebar-v2/"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+            
+                <script src="https://unpkg.com/leaflet@1.0.1/dist/leaflet.js"></script>
+                <script src="../Lelongtips/sherlock-dms/resources/components/leaflet-sidebar.js"></script>
+            
+                <script>
+                    var map = L.map('map');
+                    map.setView([51.2, 7], 9);
+            
+                    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        maxZoom: 18,
+                        attribution: 'Map data &copy; OpenStreetMap contributors'
+                    }).addTo(map);
+            
+                    var marker = L.marker([51.2, 7]).addTo(map);
+            
+                    var sidebar = L.control.sidebar('sidebar').addTo(map);
+                </script>
             </body>
             </html>
         '''
