238448920f34faece0711c9ddc41e7fdc126cb98
r.a.ling <r.a.ling@accenture.com>
2023-06-08 17:42:29 +0800

    Update map html

--- Lelongtips/sherlock-dms/resources/restAPI/Main/Main.py
+++ Lelongtips/sherlock-dms/resources/restAPI/Main/Main.py
@@ -384,13 +384,13 @@ class Main(object):
                     if price < 300000 and not i['tags'] == 'attention':
                         i['tags'] = "lrd"
                         # add_marker = marker_cluster_lrd
-                        marker_cluster_lrd.add_child(
-                            folium.Marker(
-                                location=(location['latitude'], location['longitude']),
-                                popup=popup,
-                                icon=div_icon
-                            )
-                        )
+                        # marker_cluster_lrd.add_child(
+                        #     folium.Marker(
+                        #         location=(location['latitude'], location['longitude']),
+                        #         popup=popup,
+                        #         icon=div_icon
+                        #     )
+                        # )
 
                     if i['tags'] == "attention":
                         if i['listing_status']:
@@ -404,7 +404,7 @@ class Main(object):
                             #             <div>
                             #             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-25 -25 550.00 550.00" xml:space="preserve" width="32px" height="32px" fill="#000000" transform="rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="100"> <style type="text/css"> .st0{{fill:#0d0d0d;stroke:#0d0d0d;stroke-width:50;stroke-miterlimit:10;}} .st1{{fill:#FFED1F;}} .st2{{fill:#E32B43;}} </style> <g id="border"> <path class="st0" d="M454.7,403.9L266.2,77.4c-7.2-12.4-25.1-12.4-32.3,0L45.3,403.9c-7.2,12.4,1.8,28,16.2,28h377.1 C452.9,431.9,461.9,416.4,454.7,403.9z"></path> </g> <g id="object" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg"> <g> <path class="st1" d="M232,80.6L47.2,400.7c-8,13.9,2,31.2,18,31.2h369.6c16,0,26-17.3,18-31.2L268,80.6 C260,66.7,240,66.7,232,80.6z"></path> <path class="st2" d="M250,152.2c-21.2,0-38.4,19.6-38.4,43.8c0,73.8,17.2,133.6,38.4,133.6s38.4-59.8,38.4-133.6 C288.4,171.8,271.2,152.2,250,152.2z"></path> <circle class="st2" cx="250" cy="379.9" r="26.9"></circle> </g> </g> </g><g id="SVGRepo_iconCarrier"> <style type="text/css"> .st0{{fill:#0d0d0d;stroke:#0d0d0d;stroke-width:50;stroke-miterlimit:10;}} .st1{{fill:#FFED1F;}} .st2{{fill:#E32B43;}} </style> <g id="border"> <path class="st0" d="M454.7,403.9L266.2,77.4c-7.2-12.4-25.1-12.4-32.3,0L45.3,403.9c-7.2,12.4,1.8,28,16.2,28h377.1 C452.9,431.9,461.9,416.4,454.7,403.9z"></path> </g> <g id="object" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg"> <g> <path class="st1" d="M232,80.6L47.2,400.7c-8,13.9,2,31.2,18,31.2h369.6c16,0,26-17.3,18-31.2L268,80.6 C260,66.7,240,66.7,232,80.6z"></path> <path class="st2" d="M250,152.2c-21.2,0-38.4,19.6-38.4,43.8c0,73.8,17.2,133.6,38.4,133.6s38.4-59.8,38.4-133.6 C288.4,171.8,271.2,152.2,250,152.2z"></path> <circle class="st2" cx="250" cy="379.9" r="26.9"></circle> </g> </g> </g></svg>
                             #             </div>"""
-                        add_marker = marker_cluster_attention
+                        # add_marker = marker_cluster_attention
                         marker_cluster_attention.add_child(
                             folium.Marker(
                                 location=(location['latitude'], location['longitude']),
@@ -415,7 +415,7 @@ class Main(object):
                     if price / build_up >= 700:
                         continue
 
-                    if price / build_up <= 300 and price <= 800000:
+                    if (price / build_up <= 300 and price <= 800000) or (i['tags'] == "lrd"):
                         div_icon = folium.features.CustomIcon(icon_url_durian_runtuh_lrd, icon_size=icon_size)
                         # add_marker = marker_cluster_lrd
                         marker_cluster_lrd.add_child(
